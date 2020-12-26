1e4246166818e1649ed30d33a3897ea50be9ac66
seanmichaelg1 <seanmichaelg1@gmail.com>
2020-12-26 13:12:53 +0700

    memodifikasi isi index.php

--- index.php
+++ index.php
@@ -12,4 +12,8 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<ul>
+	<li>1</li>
+	<li>2</li>
+</ul>
 </html>
